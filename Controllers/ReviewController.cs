09fb4b7ac6aad365be11a88bd138d8546d758cbf
Fikri Helmi Setiawan <fikrihelmi17@gmail.com>
2021-11-28 08:10:33 +0700

    add review

--- Controllers/ReviewController.cs
+++ Controllers/ReviewController.cs
@@ -10,9 +10,12 @@ namespace BooksCatalogue.Controllers
 {
     public class ReviewController : Controller
     {
-        private string apiEndpoint = "https://bookscatalogueapi-dicoding.azurewebsites.net/api/";
-
+        private string apiEndpoint = "https://books-catalogue-backend.azurewebsites.net/api/"; 
+        private readonly HttpClient _client;
+        public string baseUrl = "https://books-catalogue-frontend.azurewebsites.net/Books/Details/";
+    
         public ReviewController() {
+            _client = new HttpClient();
         }
 
         // GET: Review/AddReview/2
@@ -49,8 +52,32 @@ namespace BooksCatalogue.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> AddReview([Bind("Id,BookId,ReviewerName,Rating,Comment")] Review review)
         {
+                MultipartFormDataContent content = new MultipartFormDataContent();
 
-            return View(review);
+                content.Add(new StringContent(review.BookId.ToString()), "bookId");
+                content.Add(new StringContent(review.ReviewerName), "reviewerName");
+                content.Add(new StringContent(review.Rating.ToString()), "rating");
+                content.Add(new StringContent(review.Comment), "comment");
+
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, apiEndpoint + "reviews");
+                request.Content = content;
+                HttpResponseMessage response = await _client.SendAsync(request);
+
+                switch (response.StatusCode)
+                {
+                    case HttpStatusCode.OK:
+                    case HttpStatusCode.NoContent:
+                    case HttpStatusCode.Created:
+                        int bookids = review.BookId;
+                        return Redirect(baseUrl + bookids);
+                    default:
+                        return ErrorAction("Error. Status code = " + response.StatusCode + "; " + response.ReasonPhrase);
+                }
+            }
+            else 
+            {
+                return ErrorAction("Error. Status code = " + (new UnsupportedMediaTypeResult().StatusCode) + "; File is not an image.");
+            }
         }
 
         private ActionResult ErrorAction(string message)
